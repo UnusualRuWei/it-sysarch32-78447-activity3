4ab5ce922d05843c0ef16d53917b780992fe8bf7
Franz Ruie Morada <moradafranzruie@gmail.com>
2024-03-16 10:31:26 +0800

    3rd

--- it-sysarch32-78447-activity3/src/Pokemon.jsx
+++ it-sysarch32-78447-activity3/src/Pokemon.jsx
@@ -3,23 +3,61 @@
 function Pokemon() {
 
 
+  function PokemonCard() {
+    const [pokemonData, setPokemonData] = useState(null); // Stores fetched Pokemon data
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch('https://us-central1-it-sysarch32.cloudfunctions.net/pokemon');
+          if (!response.ok) {
+            throw new Error(`Error fetching Pokemon data: ${response.statusText}`);
+          }
+          const data = await response.json();
+          setPokemonData(data);
+        } catch (error) {
+          console.error('Error fetching Pokemon data:', error);
+          // Handle errors appropriately (e.g., display an error message to the user)
+        }
+      };
+    
+      fetchData(); // Call the function to fetch data on component mount
+    }, []); // Empty dependency array ensures fetching happens only once
+     
+  }
+
+  
+
     return (
       <>
         <div className="card">
-            <img className="card-image" src="" alt="Profile Pic"></img>
-            <h2 className="card-title" key="">SAmple</h2>
 
-            <div className="r-column">
-              <p>HP: {}</p>
-              <p>Attk: {}</p>
-              <p>Def: {}</p>
+          <div className="poke-info">
+              <img className="card-image" src="" alt="Profile Pic"></img>
+              <h2 className="card-title" key="">SAmple</h2>
+          </div>
+
+          <div className="poke-attri">
+              <p>Grass</p>
+              <p>Poison</p>
+          </div>
+
+          <div className="poke-stat">
+              <div className="r-column">
+                <p>HP: {}</p>
+                <p>Attk: {}</p>
+                <p>Def: {}</p>
             </div>
+
             <div className="l-column">
-              <p>Speed: {}</p>
-              <p>Sp. Attk: {}</p>
-              <p>Sp. Def: {}</p>
+                <p>Speed: {}</p>
+                <p>Sp. Attk: {}</p>
+                <p>Sp. Def: {}</p>
             </div>
+          </div>
+
         </div>
+
       </>
     )
   }
